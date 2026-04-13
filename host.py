6e9558b1188ebd890aba362a3378c76ec4447934
Farise Riefenstahl <YusatsuNao@users.noreply.github.com>
2026-04-13 17:51:41 +0700

    Repository : Official Site Update #106
    initial update for maiyome site and fixing some stuff

--- host.py
+++ host.py
@@ -2,20 +2,28 @@ from http.server import SimpleHTTPRequestHandler, HTTPServer
 import os
 
 # Configuration
-HOST = 'localhost'
-PORT = 8000
+# Note: Changed to 'localhost' to avoid the getaddrinfo error you saw earlier
+HOST = 'localhost' 
+PORT = 5201
 
 # Automatically detect the directory of the script
 DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
 class CustomHandler(SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory=DIRECTORY, **kwargs)
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+	def do_GET(self):
+		# If the user accesses the root '/', serve 'entry-test.html'
+		if self.path == '/':
+			self.path = '/index.html'
+		return super().do_GET()
 
 # Start the server
 with HTTPServer((HOST, PORT), CustomHandler) as server:
-    print(f"Serving HTTP on {HOST}:{PORT} from {DIRECTORY}")
-    try:
-        server.serve_forever()
-    except KeyboardInterrupt:
-        print("\nServer stopped.")
+	print(f"Serving HTTP on {HOST}:{PORT} from {DIRECTORY}")
+	# print(f"Main entry point: entry-test.html")
+	try:
+		server.serve_forever()
+	except KeyboardInterrupt:
+		print("\nServer stopped.")
