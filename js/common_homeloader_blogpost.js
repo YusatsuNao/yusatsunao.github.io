ac6b26303c314c93cb11199df7d56f686b2a1489
Farise Riefenstahl <YusatsuNao@users.noreply.github.com>
2026-04-20 16:13:39 +0700

    Repository : Official Site Update #126
    minor update - blog direct link update

--- js/common_homeloader_blogpost.js
+++ js/common_homeloader_blogpost.js
@@ -1,34 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
-	fetch('../js/blog_data.json')
-		.then(response => response.json())
-		.then(data => {
-			data.slice(0, 13).forEach((item, index) => { // Only process the first three items
-				createPostItem(item, index);
-			});
-		})
-		.catch(error => console.error('Error fetching data:', error));
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('../js/blog_data.json')
+        .then(response => response.json())
+        .then(data => {
+            data.slice(0, 13).forEach((item, index) => {
+                createPostItem(item, index);
+            });
+        })
+        .catch(error => console.error('Error fetching data:', error));
 
-	function createPostItem(item, index) {
-		if (item.title && item.link) {
-			const postContainer = document.querySelector('#postlist');
+    function createPostItem(item, index) {
+        if (item.title && item.link) {
+            const postContainer = document.querySelector('#postlist');
 
-			const li = document.createElement('li');
-			li.classList.add('blog', `${item.class}`, 'listed');
-			li.setAttribute('id', `link${index + 1}`);
+            const li = document.createElement('li');
+            li.classList.add('blog', `${item.class}`, 'listed');
+            li.setAttribute('id', `link${index + 1}`);
 
-			li.innerHTML = `
+            li.innerHTML = `
                 <a href="${addBlogPrefix(item.link)}">${item.id} : <span>${item.date}</span>
                 <br/>
                 <span>${item.title}</span>
                 </a>
-                 </li>
-			`;
+            `;
 
-			postContainer.appendChild(li);
-		}
-	}
+            postContainer.appendChild(li);
+        }
+    }
 
-	function addBlogPrefix(link) {
-		return link + '.html';
-	}
+    function addBlogPrefix(link) {
+        const isVercel = window.location.hostname.includes('vercel.app');
+
+        if (isVercel) {
+            return `/blog/${link}.html`;
+        }
+
+        return `${link}.html`;
+    }
 });
